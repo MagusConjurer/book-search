8ea4dbfa8afa40d3a3153b325bffdf87316d6eee
MagusConjurer <cameronisadavis@gmail.com>
2020-07-15 20:07:44 -0600

    Add dropdown menu that shows for the nav on smaller screens

--- client/src/components/CardList/index.js
+++ client/src/components/CardList/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Card() {
   return (
-    <div class="card horizontal">
-      <div class="card-image">
+    <div className="card horizontal">
+      <div className="card-image">
         <img src="https://lorempixel.com/200/200/nature/6" alt="lorem"/>
       </div>
-      <div class="card-stacked">
-        <div class="card-content">
+      <div className="card-stacked">
+        <div className="card-content">
           <span className="card-title">Book Title</span>
           <p>I am a very simple card. I am good at containing small bits of information.</p>
-          <a href="/" class="btn waves-effect waves-green teal right bottom"><i class="material-icons">save</i></a>
+          <a href="/" className="btn waves-effect waves-green teal right bottom"><i className="material-icons">save</i></a>
           </div>
       </div>
     </div>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import M from "materialize-css/dist/js/materialize.min.js";
 
-function Nav() {
-  return (
-    <nav className="grey darken-3">
-      <div className="nav-wrapper">
-        <a href="https://magusconjurer.github.io/portfolio.html" className="brand-logo">Portfolio</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="/">Search</a></li>
-          <li><a href="/saved">Saved</a></li>
-        </ul>
-      </div>
-    </nav>
-  );
+class Nav extends React.Component {
+
+  componentDidMount() {
+    // Activate the dropdown menu for when the menu shows on smaller screens
+    var dropdowns = document.querySelectorAll('.dropdown-trigger')
+    for (var i = 0; i < dropdowns.length; i++){
+      M.Dropdown.init(dropdowns[i]);
+    } 
+  }
+
+  render() {
+    return (
+      <nav className="grey darken-3">
+        <div className="nav-wrapper">
+          <a href="https://magusconjurer.github.io/portfolio.html" className="brand-logo">Portfolio</a>
+          <a href="#!" data-target="mobile-demo" className="sidenav-trigger dropdown-trigger right"><i className="material-icons">menu</i></a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="/">Search</a></li>
+            <li><a href="/saved">Saved</a></li>
+          </ul>
+          <ul className="dropdown-content" id="mobile-demo">
+            <li><a href="/">Search</a></li>
+            <li><a href="/saved">Saved</a></li>
+          </ul>
+        </div>
+      </nav>
+  )};
 };
 
 export default Nav;
